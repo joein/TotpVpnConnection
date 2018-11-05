db068a0d546d205972dc1540f8286b05efb0b304
joein <panchuk.george@outlook.com>
2018-11-05 20:46:29 +0300

    added utun check

--- TotpConnection.py
+++ TotpConnection.py
@@ -5,26 +5,57 @@ import time
 from config import Config
 
 
-def connect(str_qr_code, static_password, auth_file, username, ovpn_file):
+def connect(str_qr_code, static_password, auth_file, username, ovpn_file, utun):
     otp = pyotp.TOTP(str_qr_code).now()
     password = static_password + str(otp)
     with open(auth_file, 'w') as auth_f:
         auth_f.write(f'{username}\n')
         auth_f.write(password)
-    subprocess.Popen(['nohup', 'sudo', '-S', 'openvpn', '--config', ovpn_file])
-
-    time.sleep(30)
-
-
-connect(Config.ExternalConnection.STR_QR_CODE,
-        Config.ExternalConnection.PASSWORD,
-        Config.ExternalConnection.AUTH_FILE,
-        Config.ExternalConnection.USERNAME,
-        Config.ExternalConnection.OVPN_FILE)
+    if vpn_not_connected(utun):
+        subprocess.Popen(['sudo', 'openvpn', '--config', ovpn_file])
+        print(f'sudo openvpn --config {ovpn_file}')
+    wait_vpn_connection(utun)
 
 
-connect(Config.InternalConnection.STR_QR_CODE,
-        Config.InternalConnection.PASSWORD,
-        Config.InternalConnection.AUTH_FILE,
-        Config.InternalConnection.USERNAME,
-        Config.InternalConnection.OVPN_FILE)
+def vpn_not_connected(utun):
+    try:
+        subprocess.check_call(['ifconfig', utun])
+        print(f'{utun} is already connected')
+        return False
+    except subprocess.CalledProcessError:
+        return True
+
+
+def wait_vpn_connection(utun):
+    connected = False
+    begin = time.time()
+    while not connected and (time.time() - begin) < 60:
+        try:
+            subprocess.check_call(['ifconfig', utun])
+            connected = True
+        except subprocess.CalledProcessError:
+            time.sleep(1)
+    if not connected:
+        raise Exception(f'Connection timeout {utun}')
+    print(f'{utun} connected successfully')
+    time.sleep(2)
+    return True
+
+
+if __name__ == '__main__':
+
+    connect(Config.ExternalConnection.STR_QR_CODE,
+            Config.ExternalConnection.PASSWORD,
+            Config.ExternalConnection.AUTH_FILE,
+            Config.ExternalConnection.USERNAME,
+            Config.ExternalConnection.OVPN_FILE,
+            Config.ExternalConnection.UTUN)
+
+    connect(Config.InternalConnection.STR_QR_CODE,
+            Config.InternalConnection.PASSWORD,
+            Config.InternalConnection.AUTH_FILE,
+            Config.InternalConnection.USERNAME,
+            Config.InternalConnection.OVPN_FILE,
+            Config.InternalConnection.UTUN)
+
+
